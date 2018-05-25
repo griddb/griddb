aa81207a4986aa67b5b42fa5cb5fb9c1e12c3754
knonomura <katsuhiko.nonomura@griddb.org>
2018-05-25 15:04:36 +0900

    change copyright

--- server/service_address.cpp
+++ server/service_address.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-	Copyright (c) 2015 TOSHIBA CORPORATION.
+	Copyright (c) 2017 TOSHIBA Digital Solutions Corporation
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as
