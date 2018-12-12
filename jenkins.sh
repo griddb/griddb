69f0c202bbee3490b5caa1ce1ce181f05234bf8e
rakhighosh5 <39621328+rakhighosh5@users.noreply.github.com>
2018-12-12 14:53:49 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,5 @@
+#1/bin/bash 
+echo  "[ECHO] running"
+echo  "[INFO] Running Job" 
+echo "[INFO] File updated for POLL SCM after configuring Jenkins with CHMOD" 
+echo "[INFO] Updated for multibranch execution" 
