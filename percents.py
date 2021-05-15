9bfc6a4727be496536f137b8823afbed8803ead9
Vladvanins <73720539+Vladvanins@users.noreply.github.com>
2021-05-15 13:42:35 +0300

    Update percents.py

--- percents.py
+++ percents.py
@@ -2,10 +2,10 @@ percents = 20
 
 for i in range(percents+1):
     if 10 < i < 15 or 10 < i % 100 < 15:
-        print(f'{i} секунд')
+        print(f'{i} процентов')
     elif i % 10 == 1:
-        print(f'{i} секунда')
+        print(f'{i} процент')
     elif 1 < i % 10 < 5:
-        print(f'{i} секунды')
+        print(f'{i} процента')
     else:
-        print(f'{i} секунд')
+        print(f'{i} процентов')
